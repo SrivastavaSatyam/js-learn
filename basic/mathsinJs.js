69851d8d0f8dbc29e019bb47e3bdffbcfd9e73a8
Satyam Srivastava <71383127+SrivastavaSatyam@users.noreply.github.com>
2025-01-02 11:04:46 +0000

    Math in js

---
+++ basic/mathsinJs.js
@@ -0,0 +1,21 @@
+console.log(Math) //try in browser console 
+
+console.log(Math.abs(-4)) //return absolute value
+console.log(Math.round(40.1)) //rounds off
+console.log(Math.ceil(40.5)) //rounds up
+console.log(Math.floor(40.5)) //rounds down
+console.log(Math.min(40.5, 4, 50, 66, .3))
+console.log(Math.max(40.5, 4, 50, 66, .3))
+
+console.log(Math.random()); //always return rando values btw 0-1
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor((Math.random()*(max-min)+min))) //floor converts to lowest value 
+// Max-min+1 returns the posible range between min and max and to avoid 0, 1 is added
+//min is added to avoid begining from min val
+
+
+
