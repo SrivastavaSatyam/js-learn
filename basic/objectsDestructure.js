0397668912a86a80615798a6ce012811d3363742
Satyam Srivastava <71383127+SrivastavaSatyam@users.noreply.github.com>
2025-01-06 11:07:22 +0000

    De-structuring obbject

---
+++ basic/objectsDestructure.js
@@ -0,0 +1,10 @@
+const course = {
+    name:"js",
+    price:999,
+    Instructor:"Hfsjfsjsf"
+}
+
+// Syntax  ---> easy and good option
+const {name,Instructor} = course
+
+console.log(Instructor);
